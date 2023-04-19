6cdc3912f5d5680577e427556304fd48376ce225
Tahli Kipps <tahli.kipps@gmail.com>
2023-04-19 11:05:27 +0100

    tweaked menu css, js icon changes

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
+
 import './header.css';
 import Menu from "./menu";
 
@@ -9,23 +10,32 @@ class Header extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      burgerVisible: true,
       menuVisible: false,
+      crossVisible: false,
     };
   }
 
   showMenu = () => {
-    this.setState({
-      menuVisible: true,
-    })
+    this.setState(prevState => ({
+      menuVisible: !prevState.menuVisible,
+      burgerVisible: !prevState.burgerVisible,
+      crossVisible: !prevState.crossVisible,
+    }));
   }
 
   render() {
     return (
-      <div className="header-main">
-          <img className="tk-tarot-logo-header" src={`${process.env.PUBLIC_URL}/assets/tk-tarot-logo-header.png`} alt="logo" />
-          <span><FontAwesomeIcon className="menu-icon" icon={faBars} onClick={this.showMenu} />
-          {this.state.menuVisible ? <Menu  /> : null}
-          </span>
+      <div>
+        <div className="header-main">
+            <img className="tk-tarot-logo-header" src={`${process.env.PUBLIC_URL}/assets/tk-tarot-logo-header.png`} alt="logo" />
+
+              {this.state.burgerVisible ? <FontAwesomeIcon className="menu-icon" icon={faBars} onClick={this.showMenu} /> : null}
+              {this.state.crossVisible ? <FontAwesomeIcon className="menu-icon" icon={faXmark} onClick={this.showMenu} /> : null}
+
+
+        </div>
+        {this.state.menuVisible ? <Menu  /> : null}
       </div>
     )}
   }
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,33 +1,36 @@
 import React from 'react';
+import './menu.css';
 
 class Menu extends React.Component {
 
 
   render() {
     return (
-    <div className="Menu">
-      <ul>
-        <li>
-          <a className="menu-item" href="/">
-            Home
-          </a>
-        </li>
-        <li>
-          <a className="menu-item" href="/">
-            Seeking a Yes or No?
-          </a>
-        </li>
-        <li>
-          <a className="menu-item" href="/">
-            No3
-          </a>
-        </li>
-        <li>
-          <a className="menu-item" href="/">
-            About TEEKAY Tarot
-          </a>
-        </li>
-      </ul>
+    <div className="menu">
+      <div className="menu-container">
+        <ul className="menu-list">
+          <li>
+            <a className="menu-item" href="/">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="menu-item" href="/">
+              Seeking a Yes or No?
+            </a>
+          </li>
+          <li>
+            <a className="menu-item" href="/">
+              All cards
+            </a>
+          </li>
+          <li>
+            <a className="menu-item" href="/">
+              About TEEKAY Tarot
+            </a>
+          </li>
+        </ul>
+      </div>
   </div>
   )};
 }
