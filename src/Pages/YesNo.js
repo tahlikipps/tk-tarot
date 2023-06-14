aa60d29a672555f77c2894b53950910daa3847fa
Tahli Kipps <tahli.kipps@gmail.com>
2023-06-14 15:37:39 +0100

    minor changes to yes no page

--- src/Pages/YesNo.js
+++ src/Pages/YesNo.js
@@ -1,12 +1,9 @@
-import React, { Component } from 'react';
-import './YesNo.css';
+import React, { Component } from "react";
+import "./YesNo.css";
 import Flipper from "../components/Flipper";
 import Button from "../components/button";
 
-const delay = ms => new Promise(
-  resolve => setTimeout(resolve, ms)
-);
-
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 class YesNo extends Component {
   constructor(props) {
@@ -19,57 +16,70 @@ class YesNo extends Component {
     };
   }
 
-  selectCard = async event => {
+  selectCard = async (event) => {
     this.setState({
       flipperVisible: true,
       buttonVisible: false,
-    })
-    await delay(6000);
-    const url = "https://raw.githubusercontent.com/tahlikipps/tahlikipps.github.io/main/data.json"
-    fetch(url)
-    .then(response => response.json())
-    .then((data) => {
-      console.log(data.cards)
-      let randCard = Math.floor(Math.random() * 78)
-
-      console.log(data.cards[`${randCard}`]);
-      this.setState({
-        card: data.cards[`${randCard}`],
-        cardVisible: true,
-        flipperVisible: false,
-      });
     });
-  }
+    await delay(6000);
+    const url =
+      "https://raw.githubusercontent.com/tahlikipps/tahlikipps.github.io/main/data.json";
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.cards);
+        let randCard = Math.floor(Math.random() * 78);
 
-  render () {
+        console.log(data.cards[`${randCard}`]);
+        this.setState({
+          card: data.cards[`${randCard}`],
+          cardVisible: true,
+          flipperVisible: false,
+        });
+      });
+  };
+
+  render() {
     return (
       <div>
         <div className="main">
-
-          <div className="title">
-          {this.state.buttonVisible ? <h2>Yes or No</h2> : null}
-          </div>
-          <div className="blurb">
-            {this.state.buttonVisible ? <p>Think of a question that needs a Yes or No response, then click the button below</p> : null}
-          </div>
-          <div className="CTA-btn" onClick={this.selectCard}>
-            {this.state.buttonVisible ? <Button  /> : null}
-          </div>
-          <div className="flipper">
-            {this.state.flipperVisible ? <Flipper /> : null }
-          </div>
-          {this.state.cardVisible ?
-           <div className="yesorno">
-            <img src={this.state.card.img} alt="Selected tarot card"/>
-            <p><strong>{this.state.card.name}</strong></p>
-            <p>This card says:</p>
-            <h2>{this.state.card.YesNoMaybe}</h2>
-           </div>
-            : null}
+          {this.state.buttonVisible && (
+            <div className="title">
+              <h2>Yes or No</h2>
+            </div>
+          )}
+          {this.state.buttonVisible && (
+            <div className="blurb">
+              <p>
+                Think of a question that needs a Yes or No response, then click
+                the button below
+              </p>
+            </div>
+          )}
+          {this.state.buttonVisible && (
+            <div className="CTA-btn" onClick={this.selectCard}>
+              <Button />
+            </div>
+          )}
+          {this.state.flipperVisible && (
+            <div className="flipper">
+              <Flipper />
+            </div>
+          )}
+          {this.state.cardVisible && (
+            <div className="yesorno">
+              <img src={this.state.card.img} alt="Selected tarot card" />
+              <p>
+                <strong>{this.state.card.name}</strong>
+              </p>
+              <p>This card says:</p>
+              <h2>{this.state.card.YesNoMaybe}</h2>
+            </div>
+          )}
         </div>
       </div>
     );
-  };
+  }
 }
 
- export default YesNo;
+export default YesNo;
